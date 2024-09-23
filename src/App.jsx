64db9fe9b30e01cfe89eee7e726d999df66cffcc
Umapathi <umapathi1014@gmail.com>
2024-09-23 11:12:07 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import {React,useState} from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './Home'
 import { CaseStudy } from './CaseStudy'
 
 export const App = () => {
+    const [first, setfirst] = useState(0)
+    window.onscroll=()=>{
+    var windowscoll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (windowscoll / height) *100;
+    setfirst(scrolled);
+    };
   return (
     <>
+    <div className="scroll">
+      <div className="scrollsection" style={{width:`${first}%`}}></div>
+    </div>
     <BrowserRouter>
     <Routes>
     <Route path='/' exact element={<Home/>}/>
